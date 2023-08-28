b999be5598a04d7237d91898531394fa61eab8e2
Ozan Akkaşoğlu <ozan470@hotmail.com>
2023-08-28 21:59:13 +0300

    Time: 26 ms (33.64%), Space: 26.4 MB (95.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -1,23 +1,20 @@
 public class Solution {
     public int NumberOfSteps(int num) {
-        int i=0;
-        while(true){
-        if(num==0)
-        {
-            break;    
-        }
+        int count=0;
+        while(num>0){
+       
         if(num%2==0)
         {
         num=num/2;
-            i++;
+        count++;
         }
         else if(num%2==1)
         {
             num=num-1;
-            i++;
+        count++;
         }
     }
-       return i;
+       return count;
         
     }
 }
